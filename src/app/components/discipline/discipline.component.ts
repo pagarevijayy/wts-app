0ea6f0a5d4252bcae00546cb50ad6183b550d7ec
pagarevijayy <pagarevj97@gmail.com>
2021-04-04 18:42:54 +0530

    snackbar added and altered color for discipline

--- src/app/components/discipline/discipline.component.ts
+++ src/app/components/discipline/discipline.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-discipline',
   templateUrl: './discipline.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisciplineComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  openSnackBar(){
+    const message = "🦉 This functionality will be made available soon!";
+    this._snackBar.open(message, "", {
+      duration: 2000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center',
+    });
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 const materialListArray = [
   LayoutModule,
@@ -12,7 +13,8 @@ const materialListArray = [
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
-  MatListModule
+  MatListModule,
+  MatSnackBarModule
 ]
 
 
