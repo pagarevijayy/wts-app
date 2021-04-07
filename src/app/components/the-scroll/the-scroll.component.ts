2b23a2d962d0b0b8dc21c4728b37c67447d9ff48
pagarevijayy <pagarevj97@gmail.com>
2021-04-07 18:00:53 +0530

    pwa icon changed + in-browser-data used for scroll

--- src/app/components/the-scroll/the-scroll.component.ts
+++ src/app/components/the-scroll/the-scroll.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HostListener } from '@angular/core';
+import { IdeaQuotes } from 'src/assets/data/in-browser-data';
+
 
 @Component({
   selector: 'app-the-scroll',
@@ -14,29 +16,7 @@ export class TheScrollComponent implements OnInit {
   SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
 
   // our list of avatars
-  viewContent = [
-    {
-      viewAuthor: 'Michael Faraday',
-      viewImage: 'https://pbs.twimg.com/media/ExGzBl_UUAA3qIS?format=jpg&name=medium',
-      viewPrimaryContent: `"There’s nothing quite as frightening as someone who knows they are right."`,
-      viewSecondaryContent: 'Michael Faraday FRS was an English scientist who contributed to the study of electromagnetism and electrochemistry.',
-      visible: true
-    },
-    {
-      viewAuthor: 'George Carlin',
-      viewImage: 'https://pbs.twimg.com/media/EyCLlQfVEAARujy?format=jpg&name=small',
-      viewPrimaryContent: `"Scratch any cynic and you will find a disappointed idealist." `,
-      viewSecondaryContent: `Regarded as one of the most important and influential stand-up comics of all time, he was dubbed "the dean of counterculture comedians".`,
-      visible: false
-    },
-    {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
-      visible: false
-    }
-  ];
+  viewContent = IdeaQuotes.ideaQuotesData;
 
   constructor() { }
 
--- src/assets/data/in-browser-data.ts
+++ src/assets/data/in-browser-data.ts
@@ -1,5 +1,115 @@
 export class SourceOfTruth {
-    static projectData: any = {
-        projectMainToolBarTitle: "#wethesapiens"
+  static projectData: any = {
+    projectMainToolBarTitle: "#wethesapiens"
+  }
+}
+
+export class IdeaQuotes {
+  static ideaQuotesData: Array<any> = [
+    {
+      viewAuthor: 'Michael Faraday',
+      viewImage: 'https://pbs.twimg.com/media/ExGzBl_UUAA3qIS?format=jpg&name=medium',
+      viewPrimaryContent: `"There’s nothing quite as frightening as someone who knows they are right."`,
+      viewSecondaryContent: 'Michael Faraday FRS was an English scientist who contributed to the study of electromagnetism and electrochemistry.',
+      visible: true
+    },
+    {
+      viewAuthor: 'George Carlin',
+      viewImage: 'https://pbs.twimg.com/media/EyCLlQfVEAARujy?format=jpg&name=small',
+      viewPrimaryContent: `"Scratch any cynic and you will find a disappointed idealist." `,
+      viewSecondaryContent: `Regarded as one of the most important and influential stand-up comics of all time, he was dubbed "the dean of counterculture comedians".`,
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
+    },
+    {
+      viewAuthor: ' Thomas Edison',
+      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
+      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
+      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      visible: false
     }
+  ];
 }
