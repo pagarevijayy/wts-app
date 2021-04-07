8d22b7c37dd236055cf9c1bbeaf54413661393ec
pagarevijayy <pagarevj97@gmail.com>
2021-04-07 18:36:00 +0530

    data updated

--- src/assets/data/in-browser-data.ts
+++ src/assets/data/in-browser-data.ts
@@ -21,94 +21,115 @@ export class IdeaQuotes {
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
+      viewAuthor: 'Thomas Edison',
       viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
       viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
       viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Carl Sagan',
+      viewImage: 'https://pbs.twimg.com/media/ExLDl5MVkAUTMGz?format=jpg&name=small',
+      viewPrimaryContent: `"One glance at a book and you hear the voice of another person, perhaps someone dead for 1,000 years. To read is to voyage through time." `,
+      viewSecondaryContent: 'Carl Sagan was a scientist and educator best known for his TV series Cosmos, the Pale Blue Dot image of Earth and quotes about life.',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'J. R. R. Tolkien',
+      viewImage: 'https://pbs.twimg.com/media/ExK9ou7VoAI1Hu6?format=jpg&name=small',
+      viewPrimaryContent: `"I do not love the bright sword for its sharpness, nor the arrow for its swiftness, nor the warrior for his glory. I love only that which they defend."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Jiddu Krishnamurti',
+      viewImage: 'https://pbs.twimg.com/media/ExGjSOHUYAYBUv7?format=jpg&name=small',
+      viewPrimaryContent: `"Freedom from the desire for an answer is essential to the understanding of a problem." `,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Leo Tolstoy',
+      viewImage: 'https://pbs.twimg.com/media/Ew1f147VEAYBNrb?format=jpg&name=small',
+      viewPrimaryContent: `"Wrong does not cease to be wrong because the majority share in it."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Carl Jung',
+      viewImage: 'https://pbs.twimg.com/media/ExEGv3XVoAEQpq7?format=jpg&name=small',
+      viewPrimaryContent: `"Everything that irritates us about others can lead us to an understanding of ourselves." `,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Pablo Neruda',
+      viewImage: 'https://pbs.twimg.com/media/EwqLkvgVoAcgaQc?format=jpg&name=small',
+      viewPrimaryContent: `"You can cut all the flowers but you cannot keep Spring from coming."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'John D. Rockefeller',
+      viewImage: 'https://pbs.twimg.com/media/EwSgzmmVgAoJEtL?format=jpg&name=360x360',
+      viewPrimaryContent: `"A friendship founded on business is better than a business founded on friendship."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Daniel Kahneman',
+      viewImage: 'https://pbs.twimg.com/media/EwfzXUBVIAIbct2?format=jpg&name=360x360',
+      viewPrimaryContent: `"Intelligence is not only the ability to reason; it is also the ability to find relevant material in memory and to deploy attention when needed." `,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Henry David Thoreau',
+      viewImage: 'https://pbs.twimg.com/media/Ev20XyaU8AEf4Kx?format=jpg&name=small',
+      viewPrimaryContent: `"Disobedience is the true foundation of liberty. The obedient must be slaves."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Karl Popper',
+      viewImage: 'https://pbs.twimg.com/media/Evn1TgKVEAESCR8?format=jpg&name=small',
+      viewPrimaryContent: `"Those who promise us paradise on earth never produced anything but a hell."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Marcus Aurelius',
+      viewImage: 'https://pbs.twimg.com/media/EvlZxpLU8AA_q7U?format=jpg&name=small',
+      viewPrimaryContent: `"Reject your sense of injury and the injury itself disappears."`,
+      viewSecondaryContent: '',
       visible: false
     },
     {
-      viewAuthor: ' Thomas Edison',
-      viewImage: 'https://pbs.twimg.com/media/Exq2Fk6UUAEDqCq?format=jpg&name=small',
-      viewPrimaryContent: `"The chief function of the body is to carry the brain around."`,
-      viewSecondaryContent: 'Thomas Edison, American inventor who, singly or jointly, held a world-record 1,093 patents and created the world’s first industrial research laboratory.',
+      viewAuthor: 'Henry Ford',
+      viewImage: 'https://pbs.twimg.com/media/EvYLIZfUcAMGaKw?format=jpg&name=small',
+      viewPrimaryContent: `"You can't build a reputation on what you are going to do."`,
+      viewSecondaryContent: '',
+      visible: false
+    },
+    {
+      viewAuthor: 'Seneca',
+      viewImage: 'https://pbs.twimg.com/media/EvSn9bzUUAM38cH?format=jpg&name=small',
+      viewPrimaryContent: `"It is not the man who has too little, but the man who craves more, that is poor." `,
+      viewSecondaryContent: '',
+      visible: false
+    },
+    {
+      viewAuthor: 'George Orwell',
+      viewImage: 'https://pbs.twimg.com/media/EvJL-q-UcAAew4g?format=jpg&name=small',
+      viewPrimaryContent: `"Autobiography is only to be trusted when it reveals something disgraceful."`,
+      viewSecondaryContent: '',
+      visible: false
+    },
+    {
+      viewAuthor: 'Arthur Schopenhauer',
+      viewImage: 'https://pbs.twimg.com/media/EuSIVUUUUAMGKxp?format=jpg&name=small',
+      viewPrimaryContent: `"The person who writes for fools is always sure of a large audience."`,
+      viewSecondaryContent: '',
       visible: false
     }
   ];
